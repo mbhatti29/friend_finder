38498e6e656c54e2c10dd77985f565002671a0c2
Mudassar Bhatti <mbhatti000@citymail.cuny.edu>
2019-07-25 11:43:44 -0400

    added reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
 // import { robots } from './robot';
@@ -9,39 +9,46 @@ import ErrorBoundry from '../components/ErrorBoundry'
 // import the action and then import the connect method from react-redux
 //
 
-import { setSearchField } from '../actions'
+import { setSearchField, requestRobots } from '../actions'
 
 // tell me what peice of state i need to listen to and send it as a prop
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 // tell me what props I should listen to that are actions that need to be dispatched
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: []
-    } 
-  }
+  //dont need constructor anymore because there is no states and these robots will be returned from one of the props 'onRequestRobots()'
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: []
+  //   } 
+  // }
   
+  //! coming down as props now so dont need to make fetch call inside lifecycle methods
   componentDidMount() {
+    this.props.onRequestRobots();
     // console.log(this.props.store)
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(res => {
-        this.setState({
-          robots: res
-        })
-      })
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(res => {
+    //     this.setState({
+    //       robots: res
+    //     })
+    //   })
   }
 
   //! coming down as props so dont need to declare it as a method of app
@@ -53,13 +60,13 @@ class App extends Component {
   // }
   
   render() {
-    const {robots} = this.state;
-    const { searchField, onSearchChange } = this.props
+    // const {robots} = this.state;  // this is now coming down as props
+    const { searchField, onSearchChange, robots, isPending } = this.props
 
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })                                                        
-    return !robots.length ?
+    return isPending ?
       <h1>Loading..</h1> :
       (
         <div className='tc'>
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,38 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js'
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants'
 
-const intialState = {
+
+const intialStateSearch = {
   searchField: ''
 }
 
-export const searchRobots  = (state=intialState, action={}) => {
+export const searchRobots  = (state=intialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField: action.payload});
+    default:
+      return state;
+  }
+}
+
+const intialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const requestRobots = (state=intialStateRobots, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return {...state, isPending: true}
+    case REQUEST_ROBOTS_SUCCESS:
+      return {...state, isPending: false, robots: action.payload }
+    case REQUEST_ROBOTS_FAILED:
+      return {...state, isPending: false, error: action.payload }
     default:
       return state;
   }
